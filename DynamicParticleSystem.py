dca21ec2d1cb6c48b8a4d6031a56567e9f415a38
munucrafts <sarthakmishra520@gmail.com>
2024-08-02 11:31:54 +0530

    Few Bug Fixes

--- DynamicParticleSystem.py
+++ DynamicParticleSystem.py
@@ -3,7 +3,7 @@ import random
 import math
 
 pygame.init()
-pygame.display.set_caption('Particle System')
+pygame.display.set_caption('Dynamic Particle System')
 
 # Initialize variables
 run = True
@@ -20,22 +20,23 @@ class Polygon:
         self.position = position
         self.velocity = velocity
         self.angular_velocity = angular_velocity/100
-        self.radius = radius
-        self.poly_count = poly_count
+        self.radius = abs(radius)
+        self.poly_count = abs(poly_count)
         self.color = color
-        self.life_time = 1 / life_time
+        self.life_time = abs(1 / (life_time + 0.000001))
         self.angle = 0
 
     def update_polygon(self):
         self.position[0] += self.velocity[0]
         self.position[1] += self.velocity[1]
-        self.velocity[1] += 0.1
         self.radius -= self.life_time
         self.angle += self.angular_velocity
         if self.radius <= 0:
             polygon_array.remove(self)
 
     def draw_polygon(self):
+        if self.poly_count < 3:
+            self.poly_count = 3
         polygon_angle = 2 * math.pi / self.poly_count
         polygon_points = []
         for i in range(self.poly_count):
@@ -51,12 +52,12 @@ while run:
     
     # Set polygon properties
     new_position = [pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]]
-    new_velocity = [random.randint(0, 2), -2]
+    new_velocity = [random.randint(0, 2), 2]
+    new_angular_velocity = random.randint(-5, 5)
     new_radius = random.randint(20, 40)
     new_poly_count = 7
     new_color = (255, 255, 0)
     new_life_time = 2
-    new_angular_velocity = random.randint(-5, 5)
 
     new_polygon = Polygon(new_position, new_velocity, new_angular_velocity, new_radius, new_poly_count, new_color, new_life_time)
     polygon_array.append(new_polygon)
